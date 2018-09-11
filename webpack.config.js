d5b4c407a9705811e2aa61e7db9c9554498cb963
gerardketuma <gketuma@gmail.com>
2018-09-11 01:10:47 -0500

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path")
+const webpack = require("webpack")
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: "style-loader" },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+}
